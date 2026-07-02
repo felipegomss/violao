f51583255a001281991628374c92b802baf0ec96
Felipe Gomes <code@lfng.dev>
2026-07-02 16:16:47 -0300

    feat: índices nas FKs (Recording/PracticeSession/RepertoireSong.songId)

---
+++ prisma/migrations/20260702191632_add_fk_indexes/migration.sql
@@ -0,0 +1,8 @@
+-- CreateIndex
+CREATE INDEX "PracticeSession_songId_idx" ON "PracticeSession"("songId");
+
+-- CreateIndex
+CREATE INDEX "Recording_songId_idx" ON "Recording"("songId");
+
+-- CreateIndex
+CREATE INDEX "RepertoireSong_songId_idx" ON "RepertoireSong"("songId");
